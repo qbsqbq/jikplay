68313bdc27432bab405fd860e3566d748b0dc96f
qbsqbq <18161695029@163.com>
2020-05-21 21:53:38 +0800

    Initial Commit

---
+++ MyIjkplay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyIjkplay
+//
+//  Created by qbsqbq on 2020/5/21.
+//  Copyright © 2020 qbsqbq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
